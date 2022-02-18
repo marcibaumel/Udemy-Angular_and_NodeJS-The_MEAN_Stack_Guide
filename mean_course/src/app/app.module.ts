2c3ca437e01d4917b395c293ca76babc343bf32f
Baumel Márton <marcibaumel@gmail.com>
2022-02-18 13:36:56 +0100

    20

--- mean_course/src/app/app.module.ts
+++ mean_course/src/app/app.module.ts
@@ -3,6 +3,10 @@ import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 import { MatInputModule } from '@angular/material/input'
+import { MatCardModule } from '@angular/material/card'
+import { MatButtonModule } from '@angular/material/button'
+
+
 
 import { AppComponent } from './app.component';
 import {PostCreateComponent} from './posts/post-create/post-create.component';
@@ -16,7 +20,9 @@ import {PostCreateComponent} from './posts/post-create/post-create.component';
     BrowserModule,
     FormsModule,
     BrowserAnimationsModule,
-    MatInputModule
+    MatInputModule,
+    MatCardModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
