dcdd142857e8bce9d08630c11f8470a17c823b26
Baumel Márton <marcibaumel@gmail.com>
2022-02-19 20:38:36 +0100

    f

--- mean_course/src/app/app.component.ts
+++ mean_course/src/app/app.component.ts
@@ -1,15 +1,15 @@
 import { Component } from '@angular/core';
 
-import { Post } from './posts/post.model'
+import { Post } from './posts/post.model';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   storedPosts: Post[] = [];
 
-  onPostAdded(post){
-    this. storedPosts.push(post);
+  onPostAdded(post) {
+    this.storedPosts.push(post);
   }
 }
