f0aa0076d0f9752802e3b938c75c008c9a30205c
Baumel Márton <marcibaumel@gmail.com>
2022-02-20 11:46:09 +0100

    28

--- mean_course/src/app/posts/post-list/post-list.component.ts
+++ mean_course/src/app/posts/post-list/post-list.component.ts
@@ -1,18 +1,27 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnDestroy, OnInit } from '@angular/core';
 import { Post } from '../post.model';
 import { PostServices } from '../post.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-post-list',
   templateUrl: './post-list.component.html',
   styleUrls: ['./post-list.component.css'],
 })
-export class PostListComponenet implements OnInit{
-  @Input() posts: Post[] = [];
+export class PostListComponenet implements OnInit, OnDestroy {
+  posts: Post[] = [];
+  private postsSub: Subscription;
 
-  constructor(public postServices: PostServices){ }
+  constructor(public postServices: PostServices) {}
 
   ngOnInit(): void {
-    this. posts = this.postServices.getPosts();
+    this.posts = this.postServices.getPosts();
+    this.postsSub = this.postServices.getPostUpdateListener().subscribe((posts: Post[]) => {
+      this.posts = posts;
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.postsSub.unsubscribe();
   }
 }
