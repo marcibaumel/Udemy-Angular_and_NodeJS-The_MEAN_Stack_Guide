6a7df872edb5b5c70f9b6c3d9a5a76deb0ca2068
Baumel Márton <marcibaumel@gmail.com>
2022-02-22 18:12:36 +0100

    56

--- mean_course/src/app/posts/post.service.ts
+++ mean_course/src/app/posts/post.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { Subject } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 import { Post } from './post.model';
-
+import { map } from 'rxjs/operators';
 @Injectable({ providedIn: 'root' })
 export class PostServices {
   private posts: Post[] = [];
@@ -12,12 +12,21 @@ export class PostServices {
 
   getPosts() {
     this.http
-      .get<{ message: string; posts: Post[] }>(
+      .get<{ message: string; posts: any }>(
         'http://localhost:3000/api/posts'
       )
+      .pipe(map((postData)=>{
+        return postData.posts.map(post => {
+          return{
+            title: post.title,
+            content: post.content,
+            id: post._id
+          }
+        })
+      }))
 
-      .subscribe((postData) => {
-        this.posts = postData.posts;
+      .subscribe((transformedPosts) => {
+        this.posts = transformedPosts;
         this.postsUpdated.next([...this.posts]);
       });
   }
