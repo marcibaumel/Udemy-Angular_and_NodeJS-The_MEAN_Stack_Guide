234ddc8b460867797768be2191dd96ad2f452cd4
Baumel Márton <marcibaumel@gmail.com>
2022-02-22 16:35:46 +0100

    51

--- mean_course/backend/models/post.js
+++ mean_course/backend/models/post.js
@@ -1,1 +1,7 @@
 const mongoose = require('mongoose');
+const postSchema = mongoose.Schema({
+  title: {type: String, required: true},
+  content: {type: String, default: 'No content'}
+});
+
+module.exports = mongoose.model('Post', postSchema);
