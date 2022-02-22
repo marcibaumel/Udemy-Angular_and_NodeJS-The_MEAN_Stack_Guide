517c0d5fdc4571a0eb26ef86fe31535244fd520b
Baumel Márton <marcibaumel@gmail.com>
2022-02-22 17:19:16 +0100

    55

--- mean_course/backend/app.js
+++ mean_course/backend/app.js
@@ -1,16 +1,19 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const Post = require('./models/post');
-const mongoose = require('mongoose');
+const Post = require("./models/post");
+const mongoose = require("mongoose");
 
 const app = express();
 app.use(bodyParser.json());
 
-mongoose.connect('mongodb://localhost:27017/mean_course').then(()=>{
-  console.log("Connected to the DB");
-}).catch(()=>{
-  console.log("Connection failed");
-});
+mongoose
+  .connect("mongodb://localhost:27017/mean_course")
+  .then(() => {
+    console.log("Connected to the DB");
+  })
+  .catch(() => {
+    console.log("Connection failed");
+  });
 
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
@@ -27,27 +30,25 @@ app.use((req, res, next) => {
 
 app.post("/api/posts", (req, res, next) => {
   const post = new Post({
-    title : req.body.title,
-    content : req.body.content
+    title: req.body.title,
+    content: req.body.content,
   });
-  console.log(post);
+  //console.log(post);
+  post.save();
   res.status(201).json({
     message: "Post added successfully!",
   });
 });
 
 app.get("/api/posts", (req, res, next) => {
-  const posts = [
-    { id: "tdgg", title: "First", content: "From the server" },
-    { id: "tddfdggg", title: "Second", content: "From the server" },
-    { id: "gdgtdgg", title: "Third", content: "From the server" },
-    { id: "jjtdgg", title: "Fourth", content: "From the server" },
-  ];
-
-  return res.status(200).json({
-    message: "Posts fetched succesfully",
-    posts: posts,
+  Post.find().then(documents => {
+    console.log(documents);
+    return res.status(200).json({
+      message: "Posts fetched succesfully",
+      posts: documents,
+    });
   });
+
 });
 
 module.exports = app;
