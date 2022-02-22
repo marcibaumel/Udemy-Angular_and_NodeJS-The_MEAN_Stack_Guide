67251322b6663bf934fcb7a8f9c3b5840e3a48e6
Baumel Márton <marcibaumel@gmail.com>
2022-02-22 09:45:10 +0100

    38

--- mean_course/backend/app.js
+++ mean_course/backend/app.js
@@ -1,14 +1,19 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
-app.use((req, res, next) => {
-  console.log("First middleware")
-  next();
-})
+app.use("/api/posts", (req, res, next) => {
+  const posts = [
+    { id: "tdgg", title: "First", content: "From the server" },
+    { id: "tddfdggg", title: "Second", content: "From the server" },
+    { id: "gdgtdgg", title: "Third", content: "From the server" },
+    { id: "jjtdgg", title: "Fourth", content: "From the server" },
+  ];
 
-app.use((req, res, next) => {
-  res.send("Hello from express");
-})
+  return res.status(200).json({
+    message: "Posts fetched succesfully",
+    posts: posts,
+  });
 
+});
 
 module.exports = app;
--- mean_course/src/app/app.module.ts
+++ mean_course/src/app/app.module.ts
@@ -7,12 +7,12 @@ import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatExpansionModule } from '@angular/material/expansion';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { PostCreateComponent } from './posts/post-create/post-create.component';
 import { HeaderComponent } from './header/header.component';
 import { PostListComponenet } from './posts/post-list/post-list.component';
-
 
 @NgModule({
   declarations: [
