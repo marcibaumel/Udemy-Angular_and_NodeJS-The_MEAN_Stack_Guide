98d444b0c70768632f8ed200111a692b6f229cbd
Baumel Márton <marcibaumel@gmail.com>
2022-02-24 19:09:18 +0100

    f

--- mean_course/src/app/posts/post.service.ts
+++ mean_course/src/app/posts/post.service.ts
@@ -7,6 +7,7 @@ import { map } from 'rxjs/operators';
 export class PostServices {
   private posts: Post[] = [];
   private postsUpdated = new Subject<Post[]>();
+
   constructor(private http: HttpClient) {}
 
   getPosts() {
