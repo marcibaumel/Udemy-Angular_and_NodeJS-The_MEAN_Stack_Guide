91934ebb2c13eabd0664416593e778b0d870cee3
Baumel Márton <marcibaumel@gmail.com>
2022-02-24 15:56:27 +0100

    59

--- mean_course/backend/app.js
+++ mean_course/backend/app.js
@@ -33,10 +33,11 @@ app.post("/api/posts", (req, res, next) => {
     title: req.body.title,
     content: req.body.content,
   });
-  //console.log(post);
-  post.save();
-  res.status(201).json({
-    message: "Post added successfully!",
+  post.save().then(createdPost => {
+    res.status(201).json({
+      message: "Post added successfully!",
+      postId: createdPost._id
+    });
   });
 });
 
--- mean_course/src/app/posts/post.service.ts
+++ mean_course/src/app/posts/post.service.ts
@@ -38,9 +38,11 @@ export class PostServices {
   addPost(title: string, content: string) {
     const post: Post = { id: null, title: title, content: content };
     this.http
-      .post<{ message: string }>('http://localhost:3000/api/posts', post)
+      .post<{ message: string, postId: string }>('http://localhost:3000/api/posts', post)
       .subscribe((responseData) => {
-        console.log(responseData.message);
+        const id = responseData.postId
+        //console.log(responseData.message);
+        post.id = id;
         this.posts.push(post);
         this.postsUpdated.next([...this.posts]);
       });
@@ -48,7 +50,9 @@ export class PostServices {
 
   deletePost(postId: string){
     this.http.delete("http://localhost:3000/api/posts/"+postId).subscribe(()=>{
-      console.log("deleted");
+      const updatedPosts = this.posts.filter(post => post.id !== postId);
+      this.posts = updatedPosts;
+      this.postsUpdated.next([...this.posts]);
     });
   }
 }
