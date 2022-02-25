7ee1d66e147fc387200eb3462300277302bc042b
Baumel Márton <marcibaumel@gmail.com>
2022-02-25 15:26:46 +0100

    69

--- mean_course/backend/app.js
+++ mean_course/backend/app.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const Post = require("./models/post");
 const mongoose = require("mongoose");
 
+const postRoutes = require('./routes/posts');
+
 const app = express();
+
 app.use(bodyParser.json());
 
 mongoose
@@ -28,58 +30,6 @@ app.use((req, res, next) => {
   next();
 });
 
-app.post("/api/posts", (req, res, next) => {
-  const post = new Post({
-    title: req.body.title,
-    content: req.body.content,
-  });
-  post.save().then((createdPost) => {
-    res.status(201).json({
-      message: "Post added successfully!",
-      postId: createdPost._id,
-    });
-  });
-});
-
-app.put("/api/posts/:id", (req, res, next)=> {
-  const newPost = new Post ({
-    _id: req.body.id,
-    title: req.body.title,
-    content: req.body.content
-  })
-  Post.updateOne({_id: req.params.id}, newPost).then(result => {
-    console.log(result);
-    res.status(200).json({message: 'Update successful!'})
-  })
-});
-
-app.get('/api/posts/:id', (req,res,next)=> {
-  Post.findById(req.params.id).then(post => {
-    if(post){
-      res.status(200).json(post);
-    }
-    else{
-      res.status(404).json({message: 'Post not found'})
-    }
-  });
-});
-
-app.get("/api/posts", (req, res, next) => {
-  Post.find().then((documents) => {
-    console.log(documents);
-    return res.status(200).json({
-      message: "Posts fetched succesfully",
-      posts: documents,
-    });
-  });
-});
-
-app.delete("/api/posts/:id", (req, res, next) => {
-  //console.log(req.params.id);
-  Post.deleteOne({ _id: req.params.id }).then((result) => {
-    console.log(result);
-    res.status(200).json({ message: "Post: deleted" });
-  });
-});
+app.use("/api/posts", postRoutes);
 
 module.exports = app;
