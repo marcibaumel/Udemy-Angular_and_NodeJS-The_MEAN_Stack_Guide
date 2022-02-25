71e327a5d0ae28ea53b7f59830c6a9774f7ceaa1
Baumel Márton <marcibaumel@gmail.com>
2022-02-25 14:14:01 +0100

    f

--- mean_course/src/app/posts/post.service.ts
+++ mean_course/src/app/posts/post.service.ts
@@ -31,15 +31,18 @@ export class PostServices {
       });
   }
 
-  getPost(id: string){
-    return {...this.posts.find(p => p.id === id)};
+  getPost(id: string) {
+    return { ...this.posts.find((p) => p.id === id) };
   }
 
-  updatePost(id: string, title: string, content: string){
-    const post: Post = {id: id, title: title, content: content};
-    this.http.put('http://localhost:3000/api/posts/' + id, post).subscribe(response => console.log(response));
+  updatePost(id: string, title: string, content: string) {
+    const post: Post = { id: id, title: title, content: content };
+    this.http
+      .put('http://localhost:3000/api/posts/' + id, post)
+      .subscribe((response) => {
+        console.log(response);
+      });
   }
-
 
   getPostUpdateListener() {
     return this.postsUpdated.asObservable();
