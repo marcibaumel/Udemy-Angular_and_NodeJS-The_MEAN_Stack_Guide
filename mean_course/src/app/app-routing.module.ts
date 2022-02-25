bfe87d61a0f669cd46cf54489eacdceecebcc1e2
Baumel Márton <marcibaumel@gmail.com>
2022-02-25 10:58:42 +0100

    62

---
+++ mean_course/src/app/app-routing.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+import { PostCreateComponent } from "./posts/post-create/post-create.component";
+import { PostListComponenet } from "./posts/post-list/post-list.component";
+
+
+const routes: Routes = [
+  {path: '', component: PostListComponenet},
+  {path: 'create', component: PostCreateComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+
+export class AppRoutingModule{
+
+}
