dcb151bb0895dc09e03943925e5a1aef812d2234
Baumel Márton <marcibaumel@gmail.com>
2022-03-02 07:27:24 +0100

    86

--- mean_course/src/app/posts/post.service.ts
+++ mean_course/src/app/posts/post.service.ts
@@ -33,42 +33,45 @@ export class PostServices {
   }
 
   getPost(id: string) {
-    return this.http.get<{ _id: string; title: string; content: string, imagePath: string }>(
-      'http://localhost:3000/api/posts/' + id
-    );
+    return this.http.get<{
+      _id: string;
+      title: string;
+      content: string;
+      imagePath: string;
+    }>('http://localhost:3000/api/posts/' + id);
   }
 
   updatePost(id: string, title: string, content: string, image: File | string) {
     let postData: Post | FormData;
-    if (typeof image === "object") {
+    if (typeof image === 'object') {
       postData = new FormData();
-      postData.append("id", id);
-      postData.append("title", title);
-      postData.append("content", content);
-      postData.append("image", image, title);
+      postData.append('id', id);
+      postData.append('title', title);
+      postData.append('content', content);
+      postData.append('image', image, title);
     } else {
       postData = {
         id: id,
         title: title,
         content: content,
-        imagePath: image
+        imagePath: image,
       };
     }
     this.http
-      .put("http://localhost:3000/api/posts/" + id, postData)
-      .subscribe(response => {
+      .put('http://localhost:3000/api/posts/' + id, postData)
+      .subscribe((response) => {
         const updatedPosts = [...this.posts];
-        const oldPostIndex = updatedPosts.findIndex(p => p.id === id);
+        const oldPostIndex = updatedPosts.findIndex((p) => p.id === id);
         const post: Post = {
           id: id,
           title: title,
           content: content,
-          imagePath: ""
+          imagePath: '',
         };
         updatedPosts[oldPostIndex] = post;
         this.posts = updatedPosts;
         this.postsUpdated.next([...this.posts]);
-        this.router.navigate(["/"]);
+        this.router.navigate(['/']);
       });
   }
 
