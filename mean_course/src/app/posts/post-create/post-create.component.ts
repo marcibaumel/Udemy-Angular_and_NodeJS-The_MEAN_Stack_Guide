1640232c4b47bf1387fb7ff855f9f8b63aed8648
Baumel Márton <marcibaumel@gmail.com>
2022-03-02 07:25:56 +0100

    84

--- mean_course/src/app/posts/post-create/post-create.component.ts
+++ mean_course/src/app/posts/post-create/post-create.component.ts
@@ -47,11 +47,12 @@ export class PostCreateComponent implements OnInit {
             id: postData._id,
             title: postData.title,
             content: postData.content,
-            imagePath: null,
+            imagePath: postData.imagePath,
           };
           this.form.setValue({
             title: this.post.title,
             content: this.post.content,
+            image: this.post.imagePath
           });
         });
       } else {
@@ -76,7 +77,8 @@ export class PostCreateComponent implements OnInit {
       this.postService.updatePost(
         this.postId,
         this.form.value.title,
-        this.form.value.content
+        this.form.value.content,
+        this.form.value.image
       );
     }
 
