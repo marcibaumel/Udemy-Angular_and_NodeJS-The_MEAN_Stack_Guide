e9e81f63d2bcab3df804df70a614e8c6a1cccea9
Baumel Márton <marcibaumel@gmail.com>
2022-03-03 10:51:21 +0100

    96

--- mean_course/src/app/auth/login/login.component.ts
+++ mean_course/src/app/auth/login/login.component.ts
@@ -1,10 +1,14 @@
-import { Component } from "@angular/core";
+import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
-  templateUrl : "./login.component.html",
-  styleUrls:['./login.component.css']
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css'],
 })
-
-export class LoginComponenet{
+export class LoginComponenet {
   isLoading = false;
+
+  onLogin(form: NgForm) {
+    console.log(form.value);
+  }
 }
