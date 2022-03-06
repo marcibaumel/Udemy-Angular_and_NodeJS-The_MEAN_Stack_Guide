0ed87516546c9bf591bb7a48ad33b6d1f197bd01
Baumel Márton <marcibaumel@gmail.com>
2022-03-06 17:24:24 +0100

    110

--- mean_course/src/app/app-routing.module.ts
+++ mean_course/src/app/app-routing.module.ts
@@ -1,14 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponenet } from './auth/login/login.component';
+import { AuthGuard } from './auth/signup/auth.guard';
 import { SignupComponenet } from './auth/signup/signup.component';
 import { PostCreateComponent } from './posts/post-create/post-create.component';
 import { PostListComponenet } from './posts/post-list/post-list.component';
 
 const routes: Routes = [
   { path: '', component: PostListComponenet },
-  { path: 'create', component: PostCreateComponent },
-  { path: 'edit/:postId', component: PostCreateComponent },
+  { path: 'create', component: PostCreateComponent, canActivate: [AuthGuard] },
+  { path: 'edit/:postId', component: PostCreateComponent, canActivate: [AuthGuard] },
   { path: 'login', component: LoginComponenet },
   { path: 'signup', component: SignupComponenet },
 ];
@@ -16,5 +17,6 @@ const routes: Routes = [
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule],
+  providers: [AuthGuard]
 })
 export class AppRoutingModule {}
