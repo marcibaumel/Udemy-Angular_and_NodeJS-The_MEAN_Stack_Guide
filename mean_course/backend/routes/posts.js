1255257881ae7ecf0c7545ec7ea1b87a101598e9
Baumel Márton <marcibaumel@gmail.com>
2022-03-15 10:56:48 +0100

    delete imgs

--- mean_course/backend/routes/posts.js
+++ mean_course/backend/routes/posts.js
@@ -1,36 +1,17 @@
 const express = require("express");
-const multer = require("multer");
+
 const checkAuth = require("../middleware/check-auth");
+const extractFile = require("../middleware/file");
 const postController = require('../controller/post');
+
 
 const router = express.Router();
 
-const MIME_TYPE_MAP = {
-  "image/png": "png",
-  "image/jpeg": "jpg",
-  "image/jpg": "jpg",
-};
-
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    const isValid = MIME_TYPE_MAP[file.mimetype];
-    let error = new Error("Invalid mime type");
-    if (isValid) {
-      error = null;
-    }
-    cb(error, "backend/images");
-  },
-  filename: (req, file, cb) => {
-    const name = file.originalname.toLowerCase().split(" ").join("-");
-    const ext = MIME_TYPE_MAP[file.mimetype];
-    cb(null, name + "-" + Date.now() + "." + ext);
-  },
-});
 
 router.post(
   "",
   checkAuth,
-  multer({ storage: storage }).single("image"),
+  extractFile,
   postController.createPost
 );
 
@@ -38,7 +19,7 @@ router.post(
 router.put(
   "/:id",
   checkAuth,
-  multer({ storage: storage }).single("image"),
+  extractFile,
   postController.updatePost
 );
 
