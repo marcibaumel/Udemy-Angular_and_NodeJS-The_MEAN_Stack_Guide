ad1b5bc82a2421336b59731072f1d9f165c060f0
Baumel Márton <marcibaumel@gmail.com>
2022-03-16 18:53:26 +0100

    134

--- mean_course/backend/controller/post.js
+++ mean_course/backend/controller/post.js
@@ -42,17 +42,18 @@ exports.updatePost =  (req, res, next) => {
     creator: req.userData.userId,
   });
   Post.updateOne({ _id: req.params.id, creator: req.userData.userId }, post)
-    .then((result) => {
-      if (result.modifiedCount > 0) {
-        res.status(200).json({ message: "Update successful!" });
+    .then(result => {
+      if (result.modifiedCount >= 0) {
+        res.status(201).json({ message: "Update successful!" });
       } else {
         res.status(401).json({ message: "No auth!" });
       }
     })
-    .catch(
+    .catch(error=>{
       res.status(500).json({
         message: "Couldn't update post!",
       })
+    }
     );
 }
 
